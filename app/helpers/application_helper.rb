ce3b8fc3dd5b9d665b9101963b8a2674ff08e2db
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2019-03-16 11:51:15 +0300

    front end katmaında düzenlemelr yapıldı.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
+
     def avatar_url(user)
         gravatar_id = Digest::MD5::hexdigest(current_user.email).downcase
         "http://gravatar.com/avatar/#{gravatar_id}.png"
     end
 
-    def title(text)
-        content_for :title, text
+    def title(page_title)
+        content_for(:title) { page_title }
     end
 
 end
