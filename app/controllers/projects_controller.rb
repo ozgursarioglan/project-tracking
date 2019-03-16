b0d02c81ed4654dc4225587e41b783203f43bee1
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2019-03-16 22:33:25 +0300

    genel düzenlemeleri yüklüyorum.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -73,6 +73,6 @@ class ProjectsController < ApplicationController
     
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :description, :company, :manager_id)
+      params.require(:project).permit(:name, :description, :company, :progress, :status, :startingdate, :enddate, :manager_id)
     end
 end
