0f2edaac8d79ff70791e9416ab8138bef7a571f4
Özgür SARIOĞLAN <ozgursarioglan@hotmail.com>
2019-03-23 20:09:11 +0300

    genel düzenlenmeler.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+  Project.create! name: "Meet Mr. Miyagi", description: "Meet Mr. Miyagi", progress: 40, manager_id: 1
+  Project.create! name: "Paint the fence", description: "Meet Mr. Miyagi", progress: 60, manager_id: 2
+  Project.create! name: "Wax the car", description: "Meet Mr. Miyagi", progress: 70, manager_id: 1
+  Project.create! name: "Sand the deck", description: "Meet Mr. Miyagi", progress: 10, manager_id: 2
